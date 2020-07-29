e2a8fa79c423a2209478a58644ca9a9ecaca0a61
Ankit Raibole <raiboleankit8@gmail.com>
2020-07-29 20:44:53 +0530

    generating report using pndas profiling and printing accuracy using linear model

--- predicting_students_grades/main.py
+++ predicting_students_grades/main.py
@@ -2,20 +2,40 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
+from pandas_profiling import ProfileReport
 
 
-d1 = pd.read_csv("student-mat.csv", sep=":")
-d2 = pd.read_csv("student-por.csv", sep=";")
+# d1 = pd.read_csv("student-mat.csv", sep=":")
+# d2 = pd.read_csv("student-por.csv", sep=";")
 
-df = d1.append(d2)
+# df = d1.append(d2)
 
 
 df = pd.read_csv("student-mat.csv", sep=";")
 print(df.head())
 
+# profile = ProfileReport(
+#     df, title="Profile Report of the students grade  Dataset", explorative=True
+# )
+# profile.to_file("StudentsGrades.html")
+
+df = df[["failures", "Medu", "Fedu", "Dalc",
+         "Walc", "absences", "G1", "G2", "G3"]]
+
+# pd.Series(np.where(df.activities.values == 'yes', 1, 0),df.index)
+# df.activities.map(dict(yes=1, no=0))
+# df["activities"] = df.activities.map({'yes': 1, 'no': 0})
+print(df.head())
+
 predict = "G3"
-x = np.array(df.drop([predict], axis=1))
+x = np.array(
+    df.drop([predict], axis=1))
 y = np.array(df[predict])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
     x, y, test_size=0.2)
+
+reg = linear_model.LinearRegression(normalize=True)
+reg.fit(x_train, y_train)
+accuracy = reg.score(x_test, y_test)
+print("accuracy score = ", accuracy)
