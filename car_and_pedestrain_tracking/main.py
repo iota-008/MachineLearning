89eaaf345873e69f1a7f61d64a119fde1f5c07e3
Ankit Raibole <raiboleankit8@gmail.com>
2020-08-07 22:50:47 +0530

    car and pedesterian detection model using opencv

---
+++ car_and_pedestrain_tracking/main.py
@@ -0,0 +1,81 @@
+import cv2
+
+# image of a car
+image_file = 'images2.jpg'
+
+# pretrained car classifier
+
+
+# read image
+img = cv2.imread(image_file)
+
+# get video
+video = cv2.VideoCapture('motorbike_dashcam.mp4')
+# car  trained modeled xml file
+carTracker = 'car_detector.xml'
+# pedestrian trained modeled xml file
+pedestrianTracker = 'pedesterian_detector.xml'
+# cascade classifier
+Car_Tracker = cv2.CascadeClassifier(carTracker)
+pedestrian_tracker = cv2.CascadeClassifier(pedestrianTracker)
+
+while True:
+
+    (read_successfull, frame) = video.read()
+    if read_successfull:
+        frame_black_n_white = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    else:
+        break
+
+    # detect car
+    car = Car_Tracker.detectMultiScale(frame_black_n_white)
+    # detect pedeterian
+    pedestrian = pedestrian_tracker.detectMultiScale(frame_black_n_white)
+
+    # printing rectangles containing cars
+    for (x, y, w, h) in car:
+        cv2.rectangle(frame, (x, y),
+                      (x + w, y + h), (0, 0, 225), 2)
+    # printing rectangles containing pedestrian
+    for (x, y, w, h) in pedestrian:
+        cv2.rectangle(frame, (x, y),
+                      (x + w, y + h), (225, 0, 0), 2)
+
+    # show image for split second
+    cv2.imshow("car image : ", frame)
+
+    # wait until a key is pressed
+    key = cv2.waitKey(1)
+    if key == 13 or key == 27:
+        break
+
+# # show image for split second
+# cv2.imshow("car image : ", img)
+
+# # wait until a key is pressed
+# cv2.waitKey()
+
+
+# # convert image to black and white
+# image_bnw = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+
+# # car classifier
+# Car_Tracker = cv2.CascadeClassifier(classifier_file)
+
+# # detect car
+# car = Car_Tracker.detectMultiScale(image_bnw)
+# print(car)
+
+# # printing rectangles containing cars
+# for (x, y, w, h) in car:
+#     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 225), 2)
+
+
+# # show image for split second
+# cv2.imshow("car image : ", img)
+
+# # wait until a key is pressed
+# cv2.waitKey()
+
+print("code executed")
--- classification_of_cars/main.py
+++ classification_of_cars/main.py
@@ -1,9 +1,11 @@
-import pandas as pd
+import pickle
+
 import numpy as np
+import pandas as pd
 import sklearn
-from sklearn.utils import shuffle
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn import linear_model, preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.utils import shuffle
 
 data = pd.read_csv("car.data")
 labelencoder = preprocessing.LabelEncoder()
@@ -21,8 +23,23 @@ y = list(cls)  # labels
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
     X, y, test_size=0.2)
 
-model = KNeighborClassifier(n_neighbor=5)
+model = KNeighborsClassifier(n_neighbors=9)
 model.fit(x_train, y_train)
 
 y_predict = model.predict(x_test)
 print(y_predict)
+
+# print("accuracy = ", accuracy)
+
+best = 0
+for i in range(100):
+    accuracy = model.score(x_test, y_test)
+    if accuracy > best:
+        best = accuracy
+        with open("cars.pickle", "wb") as f:
+            pickle.dump(model, f)
+
+better_model = open("cars.pickle", "rb")
+best_model = pickle.loads(best_model)
+best_model.predict(x_test)
+print("accuracy = ", accuracy)
